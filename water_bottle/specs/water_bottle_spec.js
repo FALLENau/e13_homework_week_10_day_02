32dc9ea5cfb4280e5f61e2be3dda50015e70a5a2
FALLENau <reece.c.jones@gmail.com>
2017-07-25 14:20:01 +0100

    nan!

--- water_bottle/specs/water_bottle_spec.js
+++ water_bottle/specs/water_bottle_spec.js
@@ -5,9 +5,45 @@ var WaterBottle = require("../water_bottle.js")
 describe("Water Bottle", function(){
 
   it("should be empty at start", function(){
-    var bottle = new WaterBottle();
+    var bottle = new WaterBottle()
     assert.strictEqual(0, bottle.volume)
-
   })
+})
 
+describe("Water Bottle", function(){
+
+  it("should be full when filled", function(){
+    var bottle = new WaterBottle()
+    bottle.fill()
+    assert.strictEqual(100, bottle.volume)
+  })
+})
+
+describe("Water Bottle", function(){
+
+  it("should be empty when emptied", function(){
+    var bottle = new WaterBottle()
+    bottle.fill()
+    bottle.empty()
+    assert.strictEqual(0, bottle.volume)
+  })
+})
+
+describe("Water Bottle", function(){
+
+  it("should be go down by 10 when drunk", function(){
+    var bottle = new WaterBottle()
+    bottle.fill()
+    bottle.drink()
+    assert.strictEqual(90, bottle.volume)
+  })
+})
+
+describe("Water Bottle", function(){
+
+  it("should not go below 0", function(){
+    var bottle = new WaterBottle()
+    bottle.drink()
+    assert.strictEqual(0, bottle.volume)
+  })
 })
--- water_bottle/water_bottle.js
+++ water_bottle/water_bottle.js
@@ -2,4 +2,22 @@ var WaterBottle = function(){
   this.volume = 0
 }
 
+WaterBottle.prototype.fill = function(){
+  this.volume = 100
+}
+WaterBottle.prototype.empty = function(){
+  this.volume = 0
+}
+WaterBottle.prototype.drink = function(){
+  if (this.volume < 10) {
+   return
+  }
+  else
+  {
+  this.volume -= 10
+  }
+}
+
+
+
 module.exports = WaterBottle
