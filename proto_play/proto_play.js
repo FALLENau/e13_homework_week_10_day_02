ec442df0236eb15adb47bca5307a4ebb7fdbf86a
FALLENau <reece.c.jones@gmail.com>
2017-07-25 11:30:51 +0100

    new Fish

---
+++ proto_play/proto_play.js
@@ -0,0 +1,28 @@
+// var myPerson = Object.create(null);
+//
+// myPerson.walk = function() {
+//   console.log("I am walking")
+// }
+//
+// myPerson.walk()
+
+// var wisePerson = {
+//   wisdom: function() {
+//     console.log("commits often")
+//   }
+// }
+//
+// var myPerson = Object.create(wisePerson)
+// myPerson.walk = function() {
+//   console.log("I am walking")
+// }
+//
+// myPerson.wisdom()//inherating wisePerson thought myPerson
+
+var Fish = function(name, color) {
+  this.name = name
+  this.color = color
+  this.swim = function() {
+    console.log("Splash!")
+  }
+}
