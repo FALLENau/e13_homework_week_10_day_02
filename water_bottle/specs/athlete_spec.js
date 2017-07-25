b6e96a95a615049fb6d5b7305a0f227f428ff2cf
FALLENau <reece.c.jones@gmail.com>
2017-07-25 15:08:33 +0100

    all lab done

--- water_bottle/specs/athlete_spec.js
+++ water_bottle/specs/athlete_spec.js
@@ -3,12 +3,41 @@ var assert = require("assert")
 var WaterBottle = require("../water_bottle.js")
 var Athlete = require("../athlete.js")
 
-beforeEach()
+
 
 describe("Athlete", function(){
 
-  it("", function(){
-    var athlete = new Athlete()
-    assert.strictEqual()
+  var bottle;
+  var athlete;
+
+  beforeEach(function() {
+    bottle = new WaterBottle()
+    athlete = new Athlete(bottle)
+  })
+
+  it("starts at 100", function(){
+    assert.strictEqual(athlete.hydration, 100)
+  })
+
+  it("distance starts at 0", function() {
+    assert.strictEqual(athlete.distance, 0)
+  })
+
+  it("ran distance", function() {
+    athlete.run(10)
+    assert.strictEqual(athlete.distance, 10)
+    assert.strictEqual(athlete.hydration, 90)
+  })
+
+  it("athlete can't run dehydrated", function() {
+    athlete.run(110)
+    assert.strictEqual(athlete.distance, 100)
+  })
+
+  it("athlete can rehydrate", function() {
+    bottle.fill()
+    athlete.run(100)
+    athlete.drink()
+    assert.strictEqual( athlete.hydration, 10 )
   })
 })
--- water_bottle/specs/water_bottle_spec.js
+++ water_bottle/specs/water_bottle_spec.js
@@ -6,44 +6,32 @@ describe("Water Bottle", function(){
 
   it("should be empty at start", function(){
     var bottle = new WaterBottle()
-    assert.strictEqual(0, bottle.volume)
+    assert.strictEqual(bottle.volume, 0)
   })
-})
-
-describe("Water Bottle", function(){
 
   it("should be full when filled", function(){
     var bottle = new WaterBottle()
     bottle.fill()
-    assert.strictEqual(100, bottle.volume)
+    assert.strictEqual(bottle.volume, 100)
   })
-})
-
-describe("Water Bottle", function(){
 
   it("should be empty when emptied", function(){
     var bottle = new WaterBottle()
     bottle.fill()
     bottle.empty()
-    assert.strictEqual(0, bottle.volume)
+    assert.strictEqual(bottle.volume, 0)
   })
-})
-
-describe("Water Bottle", function(){
 
   it("should be go down by 10 when drunk", function(){
     var bottle = new WaterBottle()
     bottle.fill()
     bottle.drink()
-    assert.strictEqual(90, bottle.volume)
+    assert.strictEqual(bottle.volume, 90)
   })
-})
-
-describe("Water Bottle", function(){
 
   it("should not go below 0", function(){
     var bottle = new WaterBottle()
     bottle.drink()
-    assert.strictEqual(0, bottle.volume)
+    assert.strictEqual(bottle.volume, 0)
   })
 })
