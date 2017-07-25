f6b43708b55c608c1eb78fecc8c3458bc0f491c8
FALLENau <reece.c.jones@gmail.com>
2017-07-25 14:22:48 +0100

    athlete

---
+++ water_bottle/specs/athlete_spec.js
@@ -0,0 +1,14 @@
+var assert = require("assert")
+
+var WaterBottle = require("../water_bottle.js")
+var Athlete = require("../athlete.js")
+
+beforeEach()
+
+describe("Athlete", function(){
+
+  it("", function(){
+    var athlete = new Athlete()
+    assert.strictEqual()
+  })
+})
